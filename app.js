fd67ded316ad508f70a1a59b7744b48464210f0f
leonardo <nardinodimarco@gmail.com>
2018-02-20 18:01:26 +0100

    modulo contatti

--- app.js
+++ app.js
@@ -11,7 +11,42 @@ var helmet = require('helmet')
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var Mailgun = require('mailgun').Mailgun;
+var mg = new Mailgun(process.env.MAILGUN_API_KEY);
+
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* GET home page. */
+app.get('/contatti', function(req, res){
+  res.render('contatti');
+}); 
+
+app.post('/contatti', function(req, res) {
+  
+  var api_key = 'key-6ad39e7389043f8d687a061ec63a85bd';
+  var domain = 'mg.lumelight.it';
+  var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+   
+  var data = {
+    from: 'Lume Lighting Team Team <postmaster@mg.lumelight.it>',
+    to: 'infolumelighting@gmail.com',
+    subject: 'Hello',
+    html: 'UserName: '+req.body.userName+
+          'Email: '+req.body.email+ 
+          'Phone: '+req.body.phone+ 
+          'Company: '+req.body.company+ 
+          'Textarea: '+req.body.textarea
+  };
+  mailgun.messages().send(data, function (error, body) {
+    console.log(body);
+    res.render('contatti', {
+      msg:'Grazie. Il messaggio è stato inviato'
+    })
+    });
+  });
 
 var expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
 app.use(session({
@@ -20,7 +55,7 @@ app.use(session({
   cookie: {
     secure: true,
     httpOnly: true,
-    domain: 'lumelighting.com',
+    domain: 'lumelight.it',
     path: 'foo/bar',
     expires: expiryDate
   }
@@ -32,8 +67,6 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
